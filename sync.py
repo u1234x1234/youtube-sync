0b664e643a5b5628c5f7ce4326780f6c76ab3d7b
u1234x1234 <u1234x1234@gmail.com>
2018-11-22 21:31:49 +0300

    Catch exceptions

--- sync.py
+++ sync.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import time
 from subprocess import check_call
@@ -9,27 +10,32 @@ ROOT_DOWNLOAD_DIR = '/downloads/'
 
 
 def download():
-    with open('/to_sync.yml', 'r') as in_file:
-        items = list(yaml.load_all(in_file))
+    try:
+        with open('/to_sync.yml', 'r') as in_file:
+            items = list(yaml.load_all(in_file))
 
-    for item in items[0]:
-        path = item['path']
-        url = item['playlist_url']
+        for item in items[0]:
+            path = item['path']
+            url = item['playlist_url']
 
-        download_dir = '{}/{}'.format(ROOT_DOWNLOAD_DIR, path)
-        if not os.path.exists(download_dir):
-            os.makedirs(download_dir)
+            download_dir = '{}/{}'.format(ROOT_DOWNLOAD_DIR, path)
+            if not os.path.exists(download_dir):
+                os.makedirs(download_dir)
 
-        args = ['youtube-dl', '-c',
-                '--socket-timeout', '10', '-i',
-                '--download-archive', '{}/downloaded.txt'.format(download_dir),
-                '--extract-audio', '--audio-format', 'mp3',
-                '-o', '{}/%(title)s.%(ext)s'.format(download_dir), url]
-        check_call(args)
+            args = ['youtube-dl', '-c',
+                    '--socket-timeout', '10', '-i',
+                    '--download-archive', '{}/downloaded.txt'.format(download_dir),
+                    '--extract-audio', '--audio-format', 'mp3',
+                    '-o', '{}/%(title)s.%(ext)s'.format(download_dir), url]
+            check_call(args)
+
+    except Exception as e:
+        logging.exception(e)
 
 
-schedule.every(24).hours.do(download)
+if __name__ == '__main__':
+    schedule.every(24).hours.do(download)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
